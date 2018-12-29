e39c829d5fd905ac1319f422025488019723a3d9
fibersel <0583463@gmail.com>
2018-12-30 00:38:57 +0300

    improved work with vote changing and vote picking

--- bot.py
+++ bot.py
@@ -45,6 +45,7 @@ def repeat_1(message):
 			chats[message.chat.id][0] =  config.states.S_REGULAR
 			markup = generate_markup(chats[message.chat.id][1])	
 			chats[message.chat.id].append(markup)
+			chats[message.chat.id].append({})
 			bot.send_message(message.chat.id, 
 				'poll:\n{}'.format(chats[message.chat.id][2]), reply_markup=markup)
 		else:
@@ -60,17 +61,27 @@ def poll_text_generator(chat_id):
 	total = sum([poll[i] for i in poll])
 	text = 'poll:\n{}'.format(chats[chat_id][2])
 	for elem in poll:
-		text += '\n\n{}\n{}%'.format(elem, poll[elem] * 100 // total)
-	return text
+		text += '\n\n{}\n{}%'.format(elem, poll[elem] * 100 // max(total, 1))
+	return text + '\n{} persons voted so far'.format(total)
 
 
 @bot.callback_query_handler(func=lambda call:True)
 def callback_inline(call):
 	if call.message:
-		ans = call.data
-		chats[call.message.chat.id][1][ans] += 1
+		user_id = call.from_user.id
+		try:
+			prev = chats[call.message.chat.id][4][user_id]
+			chats[call.message.chat.id][1][prev] -= 1
+			if prev == call.data :
+				del chats[call.message.chat.id][4][user_id]
+			else :
+				chats[call.message.chat.id][4][user_id] = call.data
+				chats[call.message.chat.id][1][call.data] += 1		
+		except KeyError:
+			chats[call.message.chat.id][4][user_id] = call.data
+			chats[call.message.chat.id][1][call.data] += 1
 		bot.edit_message_text(chat_id=call.message.chat.id, 
-			message_id=call.message.message_id, text=poll_text_generator(call.message.chat.id), reply_markup=chats[call.message.chat.id][3])
+				message_id=call.message.message_id, text=poll_text_generator(call.message.chat.id), reply_markup=chats[call.message.chat.id][3])
 
 
 if __name__ == '__main__':
